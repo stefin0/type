2ff1557762d8c74834a5c1a66e0b72c8225cec83
stefin <stefinracho@gmail.com>
2023-09-25 18:55:03 -0700

    changed text

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // The text to be typed
 const textToType =
-  "This website is Stefin Racho's Final Project for Bay Valley Tech, Module 2, Cohort 236. It is a project that helps users learn how to type efficiently, using all of their fingers Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  "This website is Stefin Racho's Final Project for Bay Valley Tech, Module 2, Cohort 236. It is a project that helps users learn how to type efficiently, using all of their fingers.";
 
 // Get the container div
 const typingText = document.getElementById("typingText");
@@ -500,4 +500,5 @@ document.addEventListener("keydown", (event) => {
     );
     resetTest();
   }
+  console.log(incorrectCount, correctCount);
 });
